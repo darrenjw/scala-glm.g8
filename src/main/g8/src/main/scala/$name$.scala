86505b329e1ede0519bd3a3bce2345b24ea63a46
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-06-16 18:45:06 +0100

    fix imports

--- src/main/g8/src/main/scala/$name$.scala
+++ src/main/g8/src/main/scala/$name$.scala
@@ -4,8 +4,10 @@ Stub for scala-glm code
 
 object Stub {
 
+  import scalaglm.Pca
+  import breeze.linalg._
+
   def main(args: Array[String]): Unit = {
-    import scalaglm.Pca
     val X = DenseMatrix((1.0,1.5),(1.5,2.0),(2.0,1.5))
     val pca = Pca(X, List("V1","V2"))
     pca.summary
